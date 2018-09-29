75c7b17f20ff591061187d4f778798d6c1f0b6f5
heyuhuan <heyuhuan@baidu.com>
2018-09-29 16:56:09 +0800

    获取管理权

--- src/main/java/com/heyuhuan/worker/Master.java
+++ src/main/java/com/heyuhuan/worker/Master.java
@@ -1,16 +1,25 @@
 package com.heyuhuan.worker;
 
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooDefs;
 import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
 
 import java.io.IOException;
+import java.util.Random;
 
 public class Master implements Watcher {
 
     ZooKeeper zk;
 
     String hostPort;
+
+    String serverId = Integer.toHexString(new Random().nextInt());
+
+    boolean isLeader = false;
 
     Master(String hostPort) {
         this.hostPort = hostPort;
@@ -29,10 +38,53 @@ public class Master implements Watcher {
         System.out.println(e);
     }
 
+    boolean checkMaster() {
+        while (true) {
+            try {
+                Stat stat = new Stat();
+                byte[] data = zk.getData("/master", false, stat);
+
+                isLeader = new String(data).equals(serverId);
+
+                return true;
+            } catch (KeeperException.NoNodeException e) {
+                // no master, so try create again
+                return false;
+            } catch (KeeperException e) {
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+
+    void runForMaster() throws InterruptedException {
+        while (true) {
+            try {
+                zk.create("/master", serverId.getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+
+                isLeader = true;
+                break;
+            } catch (KeeperException.NodeExistsException e) {
+                isLeader = false;
+                break;
+            } catch (KeeperException e) {
+            }
+            if (checkMaster()) {
+                break;
+            }
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         Master m = new Master(args[0]);
         m.startZK();
-        Thread.sleep(60000);
+        m.runForMaster();
+        if (m.isLeader) {
+            System.out.println("I'm the leader");
+            // wait for a bit
+            Thread.sleep(60000);
+        } else {
+            System.out.println("Someone else is the leader");
+        }
         m.stopZK();
     }
 }
